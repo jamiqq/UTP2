7bcc233fa571a4c23a7b8c6c7ebda4452f18682b
jamiqq <s30879@pjwstk.edu.pl>
2024-10-11 17:08:43 +0200

    Added 'Adder' class

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Adder adder = new Adder();
         System.out.println(adder.add(1,2));
 
-        Subtractor subtractor = new Sustractor();
+        Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6,3));
     }
 }
